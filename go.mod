b7cbf10e63acb2bd35b25a837892fdde08715549
Ram-eez <rameez2838@gmail.com>
2025-01-04 14:49:33 +0530

    simple html scrapper

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-scrapper
+
+go 1.23.4
--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	if resp, err := http.Get("https://en.wikipedia.org/wiki/Web_scraping"); err == nil {
+		defer resp.Body.Close()
+		if body, err := io.ReadAll(resp.Body); err == nil {
+			print(string(body))
+		}
+	}
+}
