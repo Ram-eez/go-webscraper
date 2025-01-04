cfe0e235d450fb44dc94bfb0c84d9fb637f8f338
Ram-eez <rameez2838@gmail.com>
2025-01-04 15:57:05 +0530

    link collector

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-scrapper
 
 go 1.23.4
+
+require golang.org/x/net v0.33.0
--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"net/http"
+
+	"golang.org/x/net/html"
 )
 
+//	if body, err := io.ReadAll(resp.Body); err == nil {
+//		print(string(body))
+//	}
+func findLinks(n *html.Node) (links []string) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, attr := range n.Attr {
+			if attr.Key == "href" {
+				links = append(links, attr.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = append(links, findLinks(c)...)
+	}
+
+	return links
+}
 func main() {
 	if resp, err := http.Get("https://en.wikipedia.org/wiki/Web_scraping"); err == nil {
-		defer resp.Body.Close()
-		if body, err := io.ReadAll(resp.Body); err == nil {
-			print(string(body))
+		if rootNode, err := html.Parse(resp.Body); err == nil {
+			for _, link := range findLinks(rootNode) {
+				fmt.Printf("link : %s\n", link)
+			}
 		}
 	}
 }
