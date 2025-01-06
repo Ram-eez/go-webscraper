23a400da8365d5d13fce6fb0082b45b4d07e228f
Ram-eez <rameez2838@gmail.com>
2025-01-06 19:31:56 +0530

    implemented semaphores and extension filtering

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"sync"
 
 	"golang.org/x/net/html"
@@ -15,7 +16,10 @@ func findLinks(n *html.Node) (links []string) {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, attr := range n.Attr {
 			if attr.Key == "href" {
-				links = append(links, attr.Val)
+
+				if hasValidExtension(attr.Val) {
+					links = append(links, attr.Val)
+				}
 			}
 		}
 	}
@@ -26,8 +30,22 @@ func findLinks(n *html.Node) (links []string) {
 	return links
 }
 
-func getLink(url string, wg *sync.WaitGroup) {
+func hasValidExtension(link string) bool {
+	validExtensions := []string{".jpg", ".jpeg", ".png", ".gif", ".pdf", ".docx", ".mp3", ".mp4"}
+
+	for _, extensions := range validExtensions {
+		if strings.HasSuffix(link, extensions) {
+			return true
+		}
+	}
+	return false
+}
+
+func getLink(url string, wg *sync.WaitGroup, semaphore chan struct{}) {
+
 	defer wg.Done()
+	semaphore <- struct{}{}
+	fmt.Printf("Starting %s\n", url)
 	if resp, err := http.Get(url); err == nil {
 		if rootNode, err := html.Parse(resp.Body); err == nil {
 			for _, link := range findLinks(rootNode) {
@@ -35,21 +53,29 @@ func getLink(url string, wg *sync.WaitGroup) {
 			}
 		}
 	}
+	<-semaphore
+	fmt.Printf("Finished %s\n", url)
 }
 func main() {
 
 	var wg sync.WaitGroup
 
+	concurrencyLimit := 2
+	// here using struct for the semaphore we eleminate the need to occupy any memory
+	// here semaphore is a buffered channel
+	semaphore := make(chan struct{}, concurrencyLimit)
+
 	urls := []string{
 		"https://en.wikipedia.org/wiki/Web_scraping",
 		"https://www.scrapingbee.com/blog/web-scraping-go/",
+		"https://en.wikipedia.org/wiki/Wiki",
 	}
 
 	// resultsChannel := make(chan string, len(ulrs))
 
 	for _, url := range urls {
 		wg.Add(1)
-		go getLink(url, &wg)
+		go getLink(url, &wg, semaphore)
 	}
 
 	wg.Wait()
